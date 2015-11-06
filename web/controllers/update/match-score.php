a1befaa4c09a17f35a836dd7eea1c508467a4fe9
Matt Langlois <fletchto99@gmail.com>
2015-11-06 17:17:12 -0500

    Ability to score goals

--- web/controllers/update/match-score.php
+++ web/controllers/update/match-score.php
@@ -9,3 +9,11 @@
 
 require_once __DIR__ . '/../../bootstrap.php';
 authenticate();
+
+$input = new Entity();
+$input->fromJSON();
+$input->checkKeys(['Player_ID', 'Match_ID']);
+$match = Match::getMatch($input->Match_ID);
+$player = Person::getPerson($input->Player_ID);
+$assistPlayer = isset($input->Assist_ID) ? Person::getPerson($input->Assist_ID) : null;
+$match->addGoal($player, $assistPlayer);
--- web/entities/Match.php
+++ web/entities/Match.php
@@ -35,4 +35,21 @@ class Match extends Entity {
         return $match;
     }
 
+    public function addGoal($player, $assister=null) {
+        $user = Person::user();
+        if (!$user->hasRole('Organizer')) {
+            ApplicationError("Match", "You must be a tournament organizer to add goals!");
+        }
+        if ($this->Status != 1) {
+            ApplicationError("Match", "A match must be in progress to add goals!");
+        }
+        $sql = "INSERT INTO Goals(Match_ID, Player_ID, Assist_ID)
+                VALUES (?,?,?)";
+        $dbh = Database::getInstance();
+        $sth = $dbh->prepare($sql);
+
+        $assist_ID = $assister != null ? $assister->Person_ID : null;
+        $sth->execute([$this->Match_ID, $player->Person_ID, $assist_ID]);
+    }
+
 }
