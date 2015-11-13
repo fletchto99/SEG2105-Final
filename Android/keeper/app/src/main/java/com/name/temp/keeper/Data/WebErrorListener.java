5d1e14d264da6e2f763b8d3050ec394d34ce0fc6
Matt Langlois <fletchto99@gmail.com>
2015-11-13 15:27:41 -0500

    Use API V23 Move data to it's own package Modify how the data singleton interacts with errors Created a WebErrorListener to be used with web requests Update the application manifest to request permission to access the internet

---
+++ Android/keeper/app/src/main/java/com/name/temp/keeper/Data/WebErrorListener.java
@@ -0,0 +1,9 @@
+package com.name.temp.keeper.Data;
+
+import org.json.JSONObject;
+
+public interface WebErrorListener {
+
+    void onError(JSONObject error);
+
+}
--- Android/keeper/app/src/main/java/com/name/temp/keeper/MainActivity.java
+++ Android/keeper/app/src/main/java/com/name/temp/keeper/MainActivity.java
@@ -1,19 +1,17 @@
 package com.name.temp.keeper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-import com.name.temp.keeper.Data;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
+import com.name.temp.keeper.Data.Data;
+import com.name.temp.keeper.Data.WebErrorListener;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import static com.android.volley.Response.Listener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,24 +23,39 @@ public class MainActivity extends AppCompatActivity {
 
     public void getText(View view) {
         final TextView text = (TextView) findViewById(R.id.txtDisplay);
-        JSONObject tmp = new JSONObject();
-        String controller = "Hi";
-        Response.Listener listener = new Response.Listener() {
-            @Override
-            public void onResponse(Object response) {
-                text.setText(response.toString());
-            }
-        };
-        Response.ErrorListener errorListener = new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                text.setText("Its Dead Jim");
-            }
-        };
+        JSONObject data = new JSONObject();
+        try {
 
-        Data.getInstance(this).get(tmp, controller, listener, errorListener);
-        Data.getInstance(this).remove(tmp, controller, listener, errorListener);
-        Data.getInstance(this).update(tmp, controller, listener, errorListener);
+            //TODO: Replace with form data instead of hardcoded values
+            data.put("Username", "Example3");
+            data.put("Password", "Example3");
+            data.put("First_Name", "Matt");
+            data.put("Last_Name", "Langlois");
+
+            //The endpoint which we are targeting
+            String controller = "create-user";
+
+            //How to handle an error when the endpoint generates on
+            WebErrorListener errorListener = new WebErrorListener() {
+                @Override
+                public void onError(JSONObject error) {
+                    text.setText(error.toString());
+                }
+            };
+
+            Listener<JSONObject> listener= new Listener<JSONObject>() {
+
+                public void onResponse(JSONObject response) {
+                    text.setText(response.toString());
+                }
+            };
+
+            Data.getInstance(this).update(data, controller, listener, errorListener);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 
