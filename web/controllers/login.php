cb525cd2bbd5192197171ead2d29d29a18660b07
Matt Langlois <fletchto99@gmail.com>
2015-11-29 21:59:07 -0500

    Return person information when logging in

--- web/controllers/login.php
+++ web/controllers/login.php
@@ -7,6 +7,5 @@ require_once __DIR__ . '/../bootstrap.php';
 
 logout(); //Destroy any old sessions
 Authenticate();
-
-$result = new Entity(['success'=>'You have been logged in successfully!']);
-echo $result->toJSON();
+$person = Person::user();
+echo $person->toJSON();
